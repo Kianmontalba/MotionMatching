fe1bb6fd8203b18f5c868d11244b3259216970ad
Kianmontalba <145521884+Kianmontalba@users.noreply.github.com>
2026-07-25 11:09:08 +0800

    Update debug_tools.cpp

--- editor/debug_tools.cpp
+++ editor/debug_tools.cpp
@@ -4,6 +4,7 @@
 
 #include <godot_cpp/classes/h_box_container.hpp>
 #include <godot_cpp/classes/scene_tree.hpp>
+#include <godot_cpp/classes/window.hpp>
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/variant/utility_functions.hpp>
 
